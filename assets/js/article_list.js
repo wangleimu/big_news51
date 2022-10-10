54edecee2a3473a4af11d03ec568913b25fb1b0e
wanglei <wl13696750472@163.com>
2022-10-10 14:15:44 +0800

    完成

--- assets/js/article_list.js
+++ assets/js/article_list.js
@@ -2,142 +2,123 @@ $(function () {
   const layer = layui.layer
   const form = layui.form
   const laypage = layui.laypage
-  // 定义美化时间的过滤器
-  template.defaults.imports.dataFormat = function (date) {
-    const dt = new Date(date)
 
-    let y = dt.getFullYear()
-    let m = padZero(dt.getMonth() + 1)
-    let d = padZero(dt.getDate())
-
-    let hh = padZero(dt.getHours())
-    let mm = padZero(dt.getMinutes())
-    let ss = padZero(dt.getSeconds())
-
-    return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+  // 定义过滤器
+  template.defaults.imports.formatTime = (time) => {
+    let date = new Date(time)
+    let y = date.getFullYear()
+    let m = (date.getMonth() + 1 + '').padStart(2, '0')
+    let d = (date.getDate() + '').padStart(2, '0')
+    let h = (date.getHours() + '').padStart(2, '0')
+    let mm = (date.getMinutes() + '').padStart(2, '0')
+    let ss = (date.getSeconds() + '').padStart(2, '0')
+    return `${y}-${m}-${d} ${h}:${mm}:${ss}`
   }
 
-  // 定义补零的函数
-  function padZero(n) {
-    return n > 9 ? n : '0' + n
-  }
-  // 定义一个查询的参数对象，将来请求数据的时候，
-  // 需要将请求参数对象提交到服务区
   let qs = {
-    pagenum: 1,//当前页码值（表示当前第几页）
-    pagesize: 2, //当前每页显示多少
-    cate_id: '', //当前选择的文章分类
-    state: '' //当前文章所处的状态，可选值：已发布，操作都是字符串类型
+    pagenum: 1, // 当前页码值（表示当前是第几页）
+    pagesize: 2, // 当前每页显示多少条
+    cate_id: '', // 当前选择的文章分类
+    state: '' // 当前文章所处的状态，可选值：已发布，操作 都是字符串类型
+  }
+
+  loadCateList()
+
+  function loadCateList() {
+    $.ajax({
+      method: 'GET',
+      url: '/my/cate/list',
+      success(res) {
+        if (res.code !== 0) return layer.msg('获取分类列表失败了')
+        const html = template('tpl-cate', res)
+        $('[name=cate_id]').html(html)
+        // layui 本身的特性，需要多走一步
+        form.render() // render 渲染的意思
+      }
+    })
   }
 
   // 加载文章列表
-  loadAticleList()
-  initCate()
-  // 获取文章列表数据方法
-  function loadAticleList() {
+  loadArticleList()
+
+  function loadArticleList() {
     $.ajax({
       method: 'GET',
       url: `/my/article/list?pagenum=${qs.pagenum}&pagesize=${qs.pagesize}&cate_id=${qs.cate_id}&state=${qs.state}`,
-      data: qs,
       success(res) {
         if (res.code !== 0) return layer.msg('获取文章列表失败')
-        // 使用模板引擎渲染页面的数据
-        const htmlStr = template('tpl-table', res)
-        $('tbody').empty().append(htmlStr)
-        // 调用渲染分页的方法
+        const str = template('tpl-list', res)
+        $('tbody').empty().append(str)
 
+        // 做分页效果；总数是必要条件
+        renderPager(res.total)
       }
     })
   }
 
-  // 初始化文章分类方法
-  function initCate() {
-    $.ajax({
-      method: 'GET',
-      url: '/my/article/cates',
-      success: function (res) {
-        if (res.code !== 0) {
-          return layer.msg('获取分类数据失败！')
-        }
-        // 调用模板引擎渲染分类的可选项
-        let htmlStr = template('tpl-cate', res)
-        $('[name=cate_id]').html(htmlStr)
-
-        // 通过layui重新渲染表单区域的UI结构
-        form.render()
-      }
-    })
-  }
-
-  // 为筛选表单绑定submit事件
-  $('#form-search').on('submit', function (e) {
+  $('#choose-form').on('submit', function (e) {
     e.preventDefault()
-    // 获取表单中选项的值
-    let cate_id = $('[name=cate_id]').val()
-    q.cate_id = cate_id
+    // 只需要处理一下参数，再直接调用获取列表的方法
+    qs.cate_id = $('[name=cate_id]').val()
+    qs.state = $('[name=state]').val()
 
-    let state = $('[name=state]').val()
-    q.state = state
-    loadAticleList()
+    loadArticleList()
   })
 
-
-
-
+  // 渲染分页功能
   function renderPager(total) {
+    // layerui 提供的分页组件 total pagenum: 1 pagesize: 2 这些条件加起来，组件内部可以计算出有多少页
     laypage.render({
       elem: document.getElementById('pagerWrapper'),
-      count: total,
-      limit: qs.pagenum,
-      curr: qs.pagenum,
+      count: total, // 总数
+      limit: qs.pagesize, // 每页显示多少条
+      curr: qs.pagenum, // 当前是第几页
+      layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'], // layout 就是布局的意思
+      limits: [2, 3, 5, 10],
       jump(obj, first) {
-        console.log(obj.curr, obj.limit),
-          qs.pagenum = obj.curr
+        // jump回调触发的时机：1、初次渲染分页组件的时候  2、主动切换页码值的时候
+        // jump 本身就是跳跃、跳转的意思
+        // 最新的第几页和最新的每页显示多少条(curr limit)
+        qs.pagenum = obj.curr
         qs.pagesize = obj.limit
 
+        // 如果直接进行调用的话，会导致死循环的问题
+        // 应该是用户主动切换页码值的时候去加载列表
+        // if (!first) {
+        //   loadArticleList()
+        // }
         if (typeof first === 'undefined') {
-          loadAticleList()
+          loadArticleList()
         }
       }
     })
   }
 
+  $('tbody').on('click', '.btnDelete', function () {
+    const result = confirm('您确定要删除该文章吗？')
 
-  $("tbody").on('click', '.btnDelete', function () {
-    const result = confirm('确定删除吗？')
+    let len = $('.btnDelete').length
+
     if (result) {
+      // prop attr 都是用来根据属性名称获取属性值的
+      // 固、自有属性 <img src>、开发者自行添加的属性
       const id = $(this).attr('abc')
       $.ajax({
         method: 'DELETE',
         url: `/my/article/info?id=${id}`,
         success(res) {
-          if (res.code !== 0) return layer.msg('删除文章')
-          layer.msg('删除成功')
+          if (res.code !== 0) return layer.msg('删除文章失败')
+          layer.msg('删除文章成功')
 
-          if(len === 1){
+          // 判断一下，如果当前是最后一条数据的话，需要将 pagenum - 1
+          // 获取删除按钮元素的个数
+          if (len === 1) {
+            // 如果当前都已经是第一页，就不要减了，默认是第一页就好了
             qs.pagenum = qs.pagenum === 1 ? 1 : qs.pagenum - 1
           }
-          loadAticleList()
+          loadArticleList()
         }
       })
     }
   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
